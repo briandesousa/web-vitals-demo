3df30cb401149c126ac65e430a3ac49a9a618d33
Brian De Sousa <briandesousa1@gmail.com>
2021-03-30 22:55:52 -0400

    switch to HashRouter to allow routing from GitHub pages project site

--- src/components/OurWorkPage.js
+++ src/components/OurWorkPage.js
@@ -5,7 +5,7 @@ function OurWorkPage() {
     <React.Fragment>
       <h2>Our Work</h2>
       <p>Check out our latest creations.</p>
-      <img src="/images/backyard1.jpg" width="50%" height="50%" alt="stairs leading down to swimming pool"/>
+      <img src="images/backyard1.jpg" width="50%" height="50%" alt="stairs leading down to swimming pool"/>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter basename="/web-vitals-demo">
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
