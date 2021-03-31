f862d606fff63b6ded046f79dba14fb487b75a2d
Brian De Sousa <briandesousa1@gmail.com>
2021-03-30 23:03:16 -0400

    add git version info to console output

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import HomePage from './components/HomePage';
 import OurWorkPage from './components/OurWorkPage';
 import GetEstimatePage from './components/GetEstimatePage';
 import NavBar from './components/common/NavBar';
+import GitInfo from 'react-git-info/macro';
 
 function App() {
+  const gitInfo = GitInfo();
+  console.info(`Git commit hash: ${gitInfo.commit.shortHash}, commit date: ${gitInfo.commit.date}`);
+
   return (
     <main>
         <h1>Northern Getaway Backyard Solutions</h1>
