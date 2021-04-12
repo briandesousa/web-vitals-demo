aeef8193b04c28bd589bcba9bae36cde36431495
Brian De Sousa <briandesousa1@gmail.com>
2021-04-11 21:53:50 -0400

    simulate bad CLS

--- src/components/OurWorkPage.js
+++ src/components/OurWorkPage.js
@@ -6,9 +6,12 @@ function OurWorkPage() {
     <section>
       <h2>Our Work</h2>
       <p>Check out our latest creations.</p>
-      <PortfolioImage src="images/backyard1.jpg" />
-      <PortfolioImage src="images/backyard2.jpg" />
-      <PortfolioImage src="images/backyard3.jpg" />
+      <PortfolioImage src="images/backyard1.jpg" alt="stairs leading down to pool" width="30%" renderDelay="3000" />
+      <PortfolioImage src="images/backyard2.jpg" alt="garden with bushes" width="30%" renderDelay="2500" />
+      <PortfolioImage src="images/backyard3.jpg" alt="pool chairs and garden" width="30%" renderDelay="2000"/>
+      <PortfolioImage src="images/backyard4.jpg" alt="picnic area lit up at night" width="30%" renderDelay="1500"/>
+      <PortfolioImage src="images/backyard5.jpg" alt="path leading to lake" width="30%" renderDelay="1000"/>
+      <PortfolioImage src="images/backyard6.jpg" alt="planters in side yard" width="30%" renderDelay="500"/>
     </section>
   );
 }
--- src/components/common/PortfolioImage.js
+++ src/components/common/PortfolioImage.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
+
 function PortfolioImage(props) {
+
+  const [render, setRender] = useState(false);
+
+  const imageStyle = {
+    objectFit: 'contain',
+    margin: '5px',
+    display: (render ? props.display : 'none'),
+    width: props.width,
+    height: props.height
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRender(true)
+    }, props.renderDelay);
+  }, [props.renderDelay]);
+
   return (
-      <div style={{ padding: "10px" }}>
-          <img src={props.src} width="1000px" alt="stairs leading down to swimming pool" style={{ objectFit: 'contain' }} />
-      </div>
+      <img src={`${props.src}`} alt={props.altText} style={imageStyle} />
   );
+}
+
+PortfolioImage.defaultProps = {
+  renderDelay: 5000,
+  display: 'inline-block',
+  width: '',
+  height: '',
+  altText: ''
 }
 
 export default PortfolioImage;
